9740994998a52a536c850227c4085332d49db769
Jaspreet Jhoja <jj@alumni.ubc.ca>
2022-11-07 20:28:38 -0800

    added helper funcs
    added functions to
    - rasterize GDS
    - convert img to Poly
    - write poly to GDS

--- util/util.py
+++ util/util.py
@@ -2,8 +2,13 @@ from __future__ import print_function
 import torch
 import numpy as np
 from PIL import Image
-import numpy as np
-import os
+import phidl.geometry as pg
+from copy import deepcopy
+from shapely.ops import unary_union
+import rasterio.features
+import matplotlib.pyplot as plt
+import os, cv2
+from shapely.geometry.polygon import Polygon
 
 # Converts a Tensor into a Numpy array
 # |imtype|: the desired type of the converted numpy array
@@ -98,3 +103,120 @@ class Colorize(object):
             color_image[2][mask] = self.cmap[label][2]
 
         return color_image
+
+
+def gds_to_img(filename, dirpath, poly_layers = [0, 1], true_design_size = 6.144):
+      try:
+          D = pg.import_gds(filename=dirpath / filename, cellname = None, flatten=True)
+      except ValueError as e:
+          e = str(e)
+          cell_name = e[e.find('named ')+len('named '):e.find(' already')]
+          pg.gdspy.current_library.remove(cell_name)
+          D = pg.import_gds(filename = dirpath / filename, cellname = None, flatten=True)
+
+      zx = deepcopy(D)
+
+      #check design size
+      permitted_size = true_design_size
+      design_size = [round(each, 3) for each in D.size.tolist()]
+      if(design_size[0] > permitted_size or design_size[1] > permitted_size):
+          raise ValueError("Incorrect GDS size. Design size is %s"%(design_size), f"must be less than or equal to {permitted_size}um x {permitted_size}um")
+          
+      del(D)
+
+      #remove unncessary layers but the one on Layer 0 and 1
+      zx.remove_layers(layers=[int(each) for each in list(set(poly_layers).symmetric_difference(zx.layers))])
+
+      dss = true_design_size #design space size
+      tr = 1024 #target res
+      sm = tr/dss
+      all_poly_objs = []
+      from shapely.ops import unary_union
+      for each_polygon in zx.get_polygons():
+          #center the design surrounded by empty space if smaller than design space size
+          poly_pts = [tuple((np.array([round(each,3) for each in each_vertex])-np.array([zx.xmin, zx.ymin])+np.array([(dss-zx.xsize)/2, (dss-zx.ysize)/2]))*sm) for each_vertex in each_polygon.tolist()]
+          poly_inst = Polygon(poly_pts)
+          all_poly_objs.append(poly_inst)
+
+      poly_inst = unary_union(all_poly_objs)
+
+      img = rasterio.features.rasterize([poly_inst], out_shape=(tr, tr))
+      img_pil = Image.fromarray(np.flipud(img)*255)
+
+      plot = True
+      if plot:
+        plt.imshow(img_pil)
+        plt.show()
+
+      save = True
+      if(save):
+        if(not os.path.exists(dirpath/"test_A")):
+          os.mkdir(dirpath/"test_A")
+        img_pil.save(dirpath/"test_A"/str(filename.split('.')[0]+'.jpg'))
+
+      return tr, tr, dirpath, ''
+
+
+
+def img_to_poly(img_path, fs=45, plot=True, proc_flag = 0, delta_px = 0):
+
+      src = cv2.imread(img_path, 0 )
+      src = cv2.flip(src, 0)
+      # upscale img to 6144x6144
+      dim = [each*6 for each in src.shape]
+      src = cv2.resize(src, dim, cv2.INTER_AREA)
+      # remove high freq noise
+      blur = cv2.medianBlur(src,fs)
+      thresh = cv2.threshold(blur,blur.min(),blur.max(),cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+      
+      #kernel for erosion and dilation operations
+      kernel = np.ones((3,3),np.uint8) * thresh.max()
+
+      if(proc_flag == 1):
+        thresh = cv2.erode(thresh,kernel,iterations = delta_px)
+        #erode
+      elif(proc_flag == 2):
+        #dilate
+        thresh = cv2.dilate(thresh,kernel,iterations = delta_px)
+
+      if(plot):
+        plt.figure(figsize=(7,7), dpi=300,)
+        plt.subplot(1,2,1)
+        plt.imshow(src, 'gray', interpolation='none')
+        plt.subplot(1,2,2)
+        plt.imshow(src, 'gray', interpolation='none')
+        plt.imshow(thresh, 'jet', interpolation='none', alpha=0.4)
+        plt.show()
+
+      contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+      poly_pts = []
+      
+      for c in contours:
+          poly_pts.append(np.squeeze(c).tolist())
+ 
+      return poly_pts
+
+def write_gds(polygon_v, filepath="litho_example.gds", cellname="litho", output_layers=[0]):
+  #clear gds cell library for conflicts
+  [gdspy.current_library.remove(each_cell) for each_cell in list(gdspy.current_library.cells)]
+  lib = gdspy.GdsLibrary()
+  cell = lib.new_cell(cellname)
+
+  for idx, val in enumerate(output_layers):
+    ld_litho = {"layer": val, "datatype": 0}
+
+    #declare empty lithography polygon instance
+    litho_polyinst = gdspy.Polygon([], **ld_litho)
+
+    for i in range(len(polygon_v[idx])):
+      #scale points back to microns
+      temp_pts = np.array(polygon_v[idx][i])*1e-3
+      temp_pts = temp_pts.tolist()
+      if(len(polygon_v[idx][i])>2):
+        litho_polyinst = gdspy.boolean(litho_polyinst, gdspy.Polygon(temp_pts, layer = val, datatype=0), operation = 'xor', layer = val, datatype=0)
+        
+    cell.add(litho_polyinst)
+
+  lib.write_gds(filepath)
+
